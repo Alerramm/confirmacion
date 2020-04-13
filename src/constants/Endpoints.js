2f0c00b71944a13ace0416d5aefa9610e1c62426
Alejandro Galicia <out_agaliciag@independencia.com.mx>
2020-04-13 11:38:27 -0500

    endpoints produccion

--- src/constants/Endpoints.js
+++ src/constants/Endpoints.js
@@ -1,12 +1,12 @@
 /** CONSULTA CLIENTES */
 export const CONSULTAR_VIAJES =
-	'http://www.misistema.mx/beluga/Finanzas/endpoints/confirmacion/QA/get/viajes.php';
+	'http://www.misistema.mx/beluga/Finanzas/endpoints/confirmacion/get/viajes.php';
 /** CONSULTA FACTURAS */
 export const CONSULTAR_TRAMOS =
-	'http://www.misistema.mx/beluga/Finanzas/endpoints/confirmacion/QA/get/tramos.php';
+	'http://www.misistema.mx/beluga/Finanzas/endpoints/confirmacion/get/tramos.php';
 /** CONSULTA TIPO DE VIAJES */
 export const CONSULTAR_TIPO_VIAJES =
-	'https://cors-anywhere.herokuapp.com/http://www.misistema.mx/beluga/Finanzas/endpoints/confirmacion/QA/get/tipoViajes.php';
+	'https://cors-anywhere.herokuapp.com/http://www.misistema.mx/beluga/Finanzas/endpoints/confirmacion/get/tipoViajes.php';
 /** CONFIRMACION DE VIAJE */
 export const CONFIRMACION_VIAJE =
-	'https://cors-anywhere.herokuapp.com/http://www.misistema.mx/beluga/Finanzas/endpoints/confirmacion/QA/post/confirmar.php';
+	'https://cors-anywhere.herokuapp.com/http://www.misistema.mx/beluga/Finanzas/endpoints/confirmacion/post/confirmar.php';
