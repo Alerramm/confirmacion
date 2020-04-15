54e2b8b64e584fb9e46782f0b8cdaf6a2db63000
Alejandro Galicia <out_agaliciag@independencia.com.mx>
2020-04-15 18:04:32 -0500

    change QA to Prodcution

--- src/constants/Endpoints.js
+++ src/constants/Endpoints.js
@@ -1,20 +1,13 @@
-const BASE_CONFIRMATION_URL = 'http://www.misistema.mx/beluga/Finanzas/endpoints/confirmacion/QA/';
-const BASE_CLIENT_URL = 'http://www.misistema.mx/beluga/Finanzas/endpoints/cuentas/QA/';
+const BASE_CONFIRMATION_URL = 'http://www.misistema.mx/beluga/Finanzas/endpoints/confirmacion/';
+const BASE_CLIENT_URL = 'http://www.misistema.mx/beluga/Finanzas/endpoints/cuentas/';
 
 /** CONSULTA CLIENTES */
-export const CONSULTAR_VIAJES =
-	BASE_CONFIRMATION_URL + 'get/viajes.php';
+export const CONSULTAR_VIAJES = BASE_CONFIRMATION_URL + 'get/viajes.php';
 /** CONSULTA FACTURAS */
-export const CONSULTAR_TRAMOS =
-	BASE_CONFIRMATION_URL + 'get/tramos.php';
+export const CONSULTAR_TRAMOS = BASE_CONFIRMATION_URL + 'get/tramos.php';
 /** ACTUALIZA ESTATUS FACTURA */
-export const ACTUALIZA_ESTATUS_FACTURAS =
-	BASE_CLIENT_URL  + 'put/abonar.php';
+export const ACTUALIZA_ESTATUS_FACTURAS = BASE_CLIENT_URL + 'put/abonar.php';
 /** CONSULTA TIPO DE VIAJES */
-export const CONSULTAR_TIPO_VIAJES =
-	BASE_CONFIRMATION_URL + 'get/tipoViaje.php';
+export const CONSULTAR_TIPO_VIAJES = BASE_CONFIRMATION_URL + 'get/tipoViaje.php';
 /** CONFIRMACION DE VIAJE */
-export const CONFIRMACION_VIAJE =
-	BASE_CONFIRMATION_URL + 'post/confirmar.php';
-
-
+export const CONFIRMACION_VIAJE = BASE_CONFIRMATION_URL + 'post/confirmar.php';
